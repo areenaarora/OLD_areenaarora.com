e5644557bb6b4aa9a7f8886ede2f6884db38e007
Areena Arora <areena.arora@gmail.com>
2022-06-07 12:16:43 -0400

    Update map.js

--- mapping/typology-map/map.js
+++ mapping/typology-map/map.js
@@ -2,57 +2,49 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiYXJlZW5hLWFyb3JhIiwiYSI6ImNsM3U4bXk5NzI5bGIzZ211MmJmMzNpOWkifQ.vVgiB_-ozFDEiI9ERgrq2w";
 var map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/areena-arora/cl3ugxu1u003614jzkj9maigu",
-  zoom: 2.5,
-  maxZoom: 9,
-  minZoom: 3,
-  center: [-97.5, 40],
+  style: "mapbox://styles/areena-arora/cl43fks6m001c14qj9dqz03dw",
+  zoom: 6.5,
+  center: [-85.5, 37.7],
 });
-
-mmap.on("load", function () {
-    map.addLayer({
-      id: "county_typology_outline",
-      type: "line",
+map.on("load", function () {
+  map.addLayer(
+    {
+      id: "us_typology",
+      type: "fill",
       source: {
         type: "geojson",
-        data: "data/countyTypologyCodes.geojson",
+        data: "data/map_typology.geojson",
       },
       paint: {
-        "line-color": "#ffffff",
-        "line-width": 0.3,
+        "fill-color": "#cccccc",
+        "fill-outline-color": "#000000",
       },
-    }, "waterway-label");
-  
-    map.addLayer(
-      {
-        id: "county_typology",
-        type: "fill",
-        source: {
-          type: "geojson",
-          data: "data/countyTypologyCodes.geojson",
-        },
-        minzoom: 3,
-        paint: {
-          "fill-color": [
-            "match",
-            ["get", "Economic_Type_Label"],
-            "Nonspecialized",
-            "#ab9191",
-            "Maufacturing",
-            "#6193c7",
-            "Farming",
-            "#229c38",
-            "Federal/State Government",
-            "#f8c347",
-            "Recreation",
-            "#c771ba",
-            "Mining",
-            "#76e3de",
-            "#ffffff",
-          ],
-        },
+    },
+    "county_typology"
+  );
+
+  map.addLayer(
+    {
+      id: "county_typology",
+      type: "fill",
+      source: {
+        type: "geojson",
+        data: "data/map_typology.geojson",
       },
-      "county_typology_outline"
-    );
-    
-  })
+      paint: {
+        "fill-color": [
+          "match",
+          ["get", "dominant_sector"],
+          "Farming", "#ab9191",
+          "Federal/State Government", "#6193c7",
+          "Manufacturing", "#229c38",
+          "Mining", "#f8c347",
+          "Nonspecialized", "#c771ba",
+          "Recreation", "#76e3de",
+        ],
+        "fill-outline-color": "#ffffff",
+      },
+    },
+    "us_typology"
+  );
+});
