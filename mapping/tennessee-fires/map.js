3d89eff614b32948caf59fa7eb26ad1788c80fa6
Areena Arora <areena.arora@gmail.com>
2022-06-28 15:29:02 -0400

    Update map.js

--- mapping/tennessee-fires/map.js
+++ mapping/tennessee-fires/map.js
@@ -11,65 +11,65 @@ var map = new mapboxgl.Map({
 });
 
 map.on("load", function () {
-  map.addLayer({
-    id: "outline",
-    type: "line",
-    source: {
-      type: "geojson",
-      data: "data/TNData.geojson",
-    },
-    paint: {
-      "line-color": "#ffffff",
-      "line-width": 0.7,
-    },
+    map.addLayer({
+      id: "outline",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: "data/TNData.geojson",
+      },
+      paint: {
+        "line-color": "#ffffff",
+        "line-width": 0.7,
+      },
+    });
+    map.addLayer({
+      id: "wildfire",
+      type: "fill",
+      source: {
+        type: "geojson",
+        data: "data/TNData.geojson",
+      },
+      paint: {
+        "fill-color": [
+          "match",
+          ["get", "naturalBreaks"],
+          "39% - 51%", "#F8CDAA",
+          "51% - 64%", "#FAB184",
+          "64% - 86%", "#F8936D",
+          "86% - 93%", "#EF7860",
+          "93% - 100%", "#E35B5B",
+          "#ffffff",
+        ],
+        "fill-outline-color": "#ffffff",
+      },
+    });
   });
-  map.addLayer({
-    id: "wildfire",
-    type: "fill",
-    source: {
-      type: "geojson",
-      data: "data/TNData.geojson",
-    },
-    paint: {
-      "fill-color": [
-        "match",
-        ["get", "naturalBreaks"],
-        "39% - 51%",
-        "#F8CDAA",
-        "51% - 64%",
-        "#FAB184",
-        "64% - 86%",
-        "#F8936D",
-        "86% - 93%",
-        "#EF7860",
-        "93% - 100%",
-        "#E35B5B",
-        "#ffffff",
-      ],
-      "fill-outline-color": "#ffffff",
-    },
-  });
-});
 
-// Popup on hover only
-map.on("hover", "wildfire", function (e) {
-  let county_name = e.features[0].properties["NAME_x"];
-  let exposed =
-    e.features[0].properties[
-      "Total fraction of HUs directly or indirectly exposed"
-    ];
-  exposed = (exposed * 100).toFixed(0);
-  new mapboxgl.Popup()
-    .setLngLat(e.lngLat)
-    .setHTML(
-      "<h4>" +
+  // Popup
+map.on("click", "wildfire", function (e) {
+    let county_name = e.features[0].properties["NAME_x"];
+    let exposed = e.features[0].properties["Total fraction of HUs directly or indirectly exposed"];
+    exposed = (exposed * 100).toFixed(0);
+    new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML(
+        "<h4>" +
         county_name +
-        "</hr>" +
-        "<br>" +
-        "<p>" +
-        exposed +
-        "% of housing units exposed to wildfire" +
-        "</p>"
-    )
-    .addTo(map);
-});
+          "</hr>" +
+          "<br>" +
+          "<p>" +
+          exposed +
+          "% of housing units exposed to wildfire" +
+          "</p>"
+      )
+      .addTo(map);
+  });
+  // Change the cursor to a pointer when the mouse is over the turnstileData layer.
+  map.on("mouseenter", "wildfire", function () {
+    map.getCanvas().style.cursor = "pointer";
+  });
+  // Change it back to a pointer when it leaves.
+  map.on("mouseleave", "wildfire", function () {
+    map.getCanvas().style.cursor = "";
+  });
