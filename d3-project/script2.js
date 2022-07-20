d75669b39560af738ae25776e473f8b877033dde
Areena Arora <areena.arora@gmail.com>
2022-07-20 09:08:31 -0400

    D3 WORK PLEASE

---
+++ d3-project/script2.js
@@ -0,0 +1,49 @@
+// set the dimensions and margins of the graph
+var margin = { top: 30, right: 0, bottom: 100, left: 150 },
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg_new = d3.select("#scatterplot")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("FINALhundredK.csv", function(data) {
+
+  // Add X axis
+  var x_new = d3.scaleLinear()
+    .domain([0, 300000])
+    .range([ 0, width ]);
+    svg_new.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x_new));
+
+  // Add Y axis
+  var y_new = d3.scaleLinear()
+    .domain([0, 250000])
+    .range([ height, 0]);
+    svg_new.append("g")
+    .call(d3.axisLeft(y_new));
+
+  // Color scale: give me a specie name, I return a color
+  var color = d3.scaleOrdinal()
+    .domain(["MANHATTAN", "QUEENS", "BROOKLYN", "BRONX"])
+    .range(["#440154ff", "#21908dff", "#fde725ff", "#FF00FF"])
+
+  // Add dots
+  svg_new.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x_new(d.regular_gross_paid); } )
+      .attr("cy", function (d) { return y_new(d.total_ot_paid); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
+      .style("fill", function (d) { return color(d.work_location_borough) } )
+})
---
+++ d3-project/script3.js
@@ -0,0 +1,98 @@
+// set the dimensions and margins of the graph
+var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+  width = 550 - margin.left - margin.right,
+  height = 420 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3
+  .select("#my_dataviz")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("new_df.csv", function (data) {
+  // Add X axis
+  var x = d3.scaleLinear().domain([0, 55]).range([0, width]);
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear().domain([0, 3000]).range([height, 0]);
+  svg.append("g").call(d3.axisLeft(y));
+
+  // Add a scale for bubble size
+  var z = d3.scaleLinear().domain([0, 250000]).range([4, 40]);
+
+  // Add a scale for bubble color
+  var myColor = d3
+    .scaleOrdinal()
+    .domain([
+      "POLICE DEPARTMENT",
+      "FIRE DEPARTMENT",
+      "NYC HOUSING AUTHORITY",
+      "DEPARTMENT OF CORRECTION",
+      "DEPARTMENT OF SANITATION",
+    ])
+    .range(d3.schemeSet2);
+
+  // -1- Create a tooltip div that is hidden by default:
+  var tooltip = d3
+    .select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "black")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+    .style("color", "white");
+
+  // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
+  var showTooltip = function (d) {
+    tooltip.transition().duration(200);
+    tooltip
+      .style("opacity", 1)
+      .html("Agency: " + d.agency_name)
+      .style("left", d3.mouse(this)[0] + 30 + "px")
+      .style("top", d3.mouse(this)[1] + 30 + "px");
+  };
+  var moveTooltip = function (d) {
+    tooltip
+      .style("left", d3.mouse(this)[0] + 30 + "px")
+      .style("top", d3.mouse(this)[1] + 30 + "px");
+  };
+  var hideTooltip = function (d) {
+    tooltip.transition().duration(200).style("opacity", 0);
+  };
+
+  // Add dots
+  svg
+    .append("g")
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "bubbles")
+    .attr("cx", function (d) {
+      return x(d.tenure_years);
+    })
+    .attr("cy", function (d) {
+      return y(d.ot_hours);
+    })
+    .attr("r", function (d) {
+      return z(d.total_ot_paid);
+    })
+    .style("fill", function (d) {
+      return myColor(d.agency_name);
+    })
+    .style("stroke-width", "0.5")
+
+    // -3- Trigger the functions
+    .on("mouseover", showTooltip)
+    .on("mousemove", moveTooltip)
+    .on("mouseleave", hideTooltip);
+});
